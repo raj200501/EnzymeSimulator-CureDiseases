ddea5b553a206f55373728343dd2225bf62d42be
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-12 01:08:11 -0700

    Create data_genomes.sql

---
+++ backend/database/data_genomes.sql
@@ -0,0 +1,3 @@
+-- SQL script to insert genomic data (if using another database management system)
+INSERT INTO dna_sequence (sequence, source) VALUES ('ATCG', 'Database A');
+INSERT INTO dna_sequence (sequence, source) VALUES ('GCTA', 'Database B');
